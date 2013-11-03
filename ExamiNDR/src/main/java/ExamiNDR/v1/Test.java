30cc7e4fea3d8f1def5c606c4f1dc2221c880bf7
Christina <cib5224@psu.edu>
2013-11-02 20:06:04 -0400

    Test file for Gap class added

---
+++ ExamiNDR/src/main/java/ExamiNDR/v1/Test.java
@@ -0,0 +1,14 @@
+package ExamiNDR.v1;
+
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gap gap = new Gap();
+		gap.readFile(new File("/Users/Christina/Dropbox/NDR project/10_occupancy.txt"));
+	}
+
+}
